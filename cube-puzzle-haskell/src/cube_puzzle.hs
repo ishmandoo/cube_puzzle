561d57047b659e945a0f765adb0ff27ab99cb06f
Ben <ishmandoo@gmail.com>
2019-02-27 09:33:14 -0500

    refactored

--- cube-puzzle-haskell/src/cube_puzzle.hs
+++ cube-puzzle-haskell/src/cube_puzzle.hs
@@ -1,84 +1,110 @@
 import Data.Maybe (catMaybes)
 import Data.List (find)
 
-data Face = Top | Bot | Lef | Ri | Front | Back deriving Show
-data Dir = North | South | East | West deriving Show
+data Face = Top | Bot | Lef | Ri | Front | Back deriving (Show, Eq)
+data Dir = North | South | East | West deriving (Show, Eq)
 
-data GearState = GearState{ face :: Face, dir :: Dir, tooth :: Int } deriving Show
+data GearState = GearState{ face :: Face, dir :: Dir, tooth :: Int, history :: [(Face, Dir)] } deriving Show
 
 turn :: GearState -> Maybe GearState
-turn (GearState face North tooth) = Just (GearState face East tooth)
-turn (GearState face East tooth) = Just (GearState face North tooth)
+turn (GearState face North tooth history) = Just (GearState face East tooth ((face,North):history))
+turn (GearState face East tooth history) = Just (GearState face North tooth ((face,East):history))
 
-turn (GearState face South tooth) = Just (GearState face West tooth)
-turn (GearState face West tooth) = Just (GearState face South tooth)
+turn (GearState face South tooth history) = Just (GearState face West tooth ((face,South):history))
+turn (GearState face West tooth history) = Just (GearState face South tooth ((face,West):history))
+
+connections = 
+    [ ((Top, East),(Ri, South))
+    , ((Top, West),(Lef, South))
+
+    , ((Bot, North),(Back, North))
+    , ((Bot, South),(Front, North))
+    , ((Bot, West),(Ri, North)) 
+
+    , ((Front, South),(Bot, North)) 
+    , ((Front, West),(Lef, West)) 
+
+    , ((Back, South),(Bot, South)) 
+    , ((Back, East),(Lef, East)) 
+    , ((Back, West),(Ri, West)) 
+
+    , ((Lef, North),(Top, East)) 
+    , ((Lef, East),(Front, East))
+    , ((Lef, West),(Back, West)) 
+    
+    , ((Ri, North),(Top, West)) 
+    , ((Ri, South),(Bot, East)) 
+    , ((Ri, East),(Back, East))  
+    ]
+
+reverseTuple :: (a, b) -> (b, a)
+reverseTuple (a,b) = (b,a)
+
+reverse_connections = map reverseTuple connections
 
 forward :: GearState -> Maybe GearState
-forward (GearState Top North tooth) = Nothing
-forward (GearState Top South tooth) = Nothing
-forward (GearState Top East tooth) = Just (GearState Ri South (incr tooth))
-forward (GearState Top West tooth) = Just (GearState Lef South (incr tooth))
+forward (GearState face dir tooth history) = 
+    case (lookup (face, dir) connections) of
+        Nothing -> Nothing
+        (Just (new_face, new_dir)) -> Just (GearState new_face new_dir (incr tooth) ((new_face,new_dir):history))
 
-forward (GearState Bot North tooth) = Just (GearState Front West (incr tooth))
-forward (GearState Bot South tooth) = Just (GearState Back East (incr tooth))
-forward (GearState Bot East tooth) = Nothing
-forward (GearState Bot West tooth) = Just (GearState Ri North (incr tooth))
+backward :: GearState -> Maybe GearState
+backward (GearState face dir tooth history) = 
+    case (lookup (face, dir) reverse_connections) of
+        Nothing -> Nothing
+        (Just (new_face, new_dir)) -> Just (GearState new_face new_dir (decr tooth) ((new_face,new_dir):history))
 
-forward (GearState Front North tooth) = Nothing
-forward (GearState Front South tooth) = Just (GearState Bot North (incr tooth))
-forward (GearState Front East tooth) = Nothing
-forward (GearState Front West tooth) = Just (GearState Back West (incr tooth))
+{- 
+forward :: GearState -> Maybe GearState
+forward (GearState Top East tooth history) = Just (GearState Ri South (incr tooth) ((Top,East):history))
+forward (GearState Top West tooth history) = Just (GearState Lef South (incr tooth) ((Top,West):history))
 
-forward (GearState Back North tooth) = Nothing
-forward (GearState Back South tooth) = Just (GearState Bot North (incr tooth))
-forward (GearState Back East tooth) = Just (GearState Ri East (incr tooth))
-forward (GearState Back West tooth) = Just (GearState Lef West (incr tooth))
+forward (GearState Bot North tooth history) = Just (GearState Back North (incr tooth) ((Bot,North):history))
+forward (GearState Bot South tooth history) = Just (GearState Front North (incr tooth) ((Bot,South):history))
+forward (GearState Bot West tooth history) = Just (GearState Ri North (incr tooth) ((Bot,West):history))
 
-forward (GearState Lef North tooth) = Just (GearState Top East (incr tooth))
-forward (GearState Lef South tooth) = Nothing
-forward (GearState Lef East tooth) = Just (GearState Front East (incr tooth))
-forward (GearState Lef West tooth) = Just (GearState Back West (incr tooth))
+forward (GearState Front South tooth history) = Just (GearState Bot North (incr tooth) ((Front,South):history))
+forward (GearState Front West tooth history) = Just (GearState Lef West (incr tooth) ((Front,West):history))
 
-forward (GearState Ri North tooth) = Just (GearState Top West (incr tooth))
-forward (GearState Ri South tooth) = Just (GearState Bot East (incr tooth))
-forward (GearState Ri East tooth) = Just (GearState Back East (incr tooth))
-forward (GearState Ri West tooth) = Nothing
+forward (GearState Back South tooth history) = Just (GearState Bot South (incr tooth) ((Back,South):history))
+forward (GearState Back East tooth history) = Just (GearState Lef East (incr tooth) ((Back,East):history))
+forward (GearState Back West tooth history) = Just (GearState Ri West (incr tooth) ((Back,West):history))
+
+forward (GearState Lef North tooth history) = Just (GearState Top East (incr tooth) ((Lef,North):history))
+forward (GearState Lef East tooth history) = Just (GearState Front East (incr tooth) ((Lef,East):history))
+forward (GearState Lef West tooth history) = Just (GearState Back West (incr tooth) ((Lef,West):history))
+
+forward (GearState Ri North tooth history) = Just (GearState Top West (incr tooth) ((Ri,North):history))
+forward (GearState Ri South tooth history) = Just (GearState Bot East (incr tooth) ((Ri,South):history))
+forward (GearState Ri East tooth history) = Just (GearState Back East (incr tooth) ((Ri,East):history))
 
 forward _ = Nothing
 
 backward :: GearState -> Maybe GearState
 
-backward (GearState Top South tooth) = Nothing
-backward (GearState Top North tooth) = Nothing
-backward (GearState Top West tooth) = Just (GearState Ri South (decr tooth))
-backward (GearState Top East tooth) = Just (GearState Lef South (decr tooth))
+backward (GearState Top West tooth history) = Just (GearState Ri North (decr tooth) ((Top,West):history))
+backward (GearState Top East tooth history) = Just (GearState Lef North (decr tooth) ((Top,East):history))
 
-backward (GearState Bot South tooth) = Just (GearState Back North (decr tooth))
-backward (GearState Bot North tooth) = Just (GearState Front North (decr tooth))
-backward (GearState Bot West tooth) = Nothing
-backward (GearState Bot East tooth) = Just (GearState Ri North (decr tooth))
+backward (GearState Bot South tooth history) = Just (GearState Back South (decr tooth) ((Bot,South):history))
+backward (GearState Bot North tooth history) = Just (GearState Front South (decr tooth) ((Bot,North):history))
+backward (GearState Bot East tooth history) = Just (GearState Ri South (decr tooth) ((Bot,East):history))
 
-backward (GearState Front South tooth) = Nothing
-backward (GearState Front North tooth) = Just (GearState Bot South (decr tooth))
-backward (GearState Front West tooth) = Nothing
-backward (GearState Front East tooth) = Just (GearState Back East (decr tooth))
+backward (GearState Front North tooth history) = Just (GearState Bot South (decr tooth) ((Front,North):history))
+backward (GearState Front East tooth history) = Just (GearState Lef East (decr tooth) ((Front,East):history))
 
-backward (GearState Back South tooth) = Nothing
-backward (GearState Back North tooth) = Just (GearState Bot North (decr tooth))
-backward (GearState Back West tooth) = Just (GearState Ri West (decr tooth))
-backward (GearState Back East tooth) = Just (GearState Lef East (decr tooth))
+backward (GearState Back North tooth history) = Just (GearState Bot North (decr tooth) ((Back,North):history))
+backward (GearState Back West tooth history) = Just (GearState Lef West (decr tooth) ((Back,West):history))
+backward (GearState Back East tooth history) = Just (GearState Ri East (decr tooth) ((Back,East):history))
 
-backward (GearState Lef South tooth) = Just (GearState Top West (decr tooth))
-backward (GearState Lef North tooth) = Nothing
-backward (GearState Lef West tooth) = Just (GearState Front West (decr tooth))
-backward (GearState Lef East tooth) = Just (GearState Back East (decr tooth))
+backward (GearState Lef South tooth history) = Just (GearState Top West (decr tooth) ((Lef,South):history))
+backward (GearState Lef West tooth history) = Just (GearState Front West (decr tooth) ((Lef,West):history))
+backward (GearState Lef East tooth history) = Just (GearState Back East (decr tooth) ((Lef,East):history))
 
-backward (GearState Ri South tooth) = Just (GearState Top East (decr tooth))
-backward (GearState Ri North tooth) = Just (GearState Bot West (decr tooth))
-backward (GearState Ri West tooth) = Just (GearState Back West (decr tooth))
-backward (GearState Ri East tooth) = Nothing
+backward (GearState Ri South tooth history) = Just (GearState Top East (decr tooth) ((Ri,South):history))
+backward (GearState Ri North tooth history) = Just (GearState Bot West (decr tooth) ((Ri,North):history))
+backward (GearState Ri West tooth history) = Just (GearState Back West (decr tooth) ((Ri,West):history))
 
-backward _ = Nothing
+backward _ = Nothing -}
 
 
 mod5 :: Int -> Int
@@ -91,7 +117,7 @@ decr :: Int -> Int
 decr x = mod5 (x-1)
 
 isWinningState :: GearState -> Bool
-isWinningState (GearState Top North 0) = True
+isWinningState (GearState Top North 0 _) = True
 isWinningState _ = False
 
 getNextStates :: GearState -> [GearState]
@@ -105,7 +131,7 @@ checkFrontier frontier = find isWinningState frontier
 
 search :: Int -> [GearState] -> Maybe GearState
 search 0 _ = Nothing
-search remaining frontier = 
+search depth frontier = 
     case (checkFrontier frontier) of 
-        Nothing -> (search (remaining - 1) (getNewFrontier frontier)) 
+        Nothing -> (search (depth - 1) (getNewFrontier frontier)) 
         (Just state) -> Just state
