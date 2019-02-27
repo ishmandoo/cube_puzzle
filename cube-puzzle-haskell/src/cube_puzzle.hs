ab5625d774beebee280fa24cdd35ba4e865d6fe0
Ben <ishmandoo@gmail.com>
2019-02-27 14:05:36 -0500

    tweaks

--- cube-puzzle-haskell/src/cube_puzzle.hs
+++ cube-puzzle-haskell/src/cube_puzzle.hs
@@ -6,13 +6,6 @@ data Dir = North | South | East | West deriving (Show, Eq)
 
 data GearState = GearState{ face :: Face, dir :: Dir, tooth :: Int, history :: [(Face, Dir)] } deriving Show
 
-turn :: GearState -> Maybe GearState
-turn (GearState face North tooth history) = Just (GearState face East tooth ((face,North):history))
-turn (GearState face East tooth history) = Just (GearState face North tooth ((face,East):history))
-
-turn (GearState face South tooth history) = Just (GearState face West tooth ((face,South):history))
-turn (GearState face West tooth history) = Just (GearState face South tooth ((face,West):history))
-
 connections = 
     [ ((Top, East),(Ri, South))
     , ((Top, West),(Lef, South))
@@ -54,58 +47,13 @@ backward (GearState face dir tooth history) =
         Nothing -> Nothing
         (Just (new_face, new_dir)) -> Just (GearState new_face new_dir (decr tooth) ((new_face,new_dir):history))
 
-{- 
-forward :: GearState -> Maybe GearState
-forward (GearState Top East tooth history) = Just (GearState Ri South (incr tooth) ((Top,East):history))
-forward (GearState Top West tooth history) = Just (GearState Lef South (incr tooth) ((Top,West):history))
 
-forward (GearState Bot North tooth history) = Just (GearState Back North (incr tooth) ((Bot,North):history))
-forward (GearState Bot South tooth history) = Just (GearState Front North (incr tooth) ((Bot,South):history))
-forward (GearState Bot West tooth history) = Just (GearState Ri North (incr tooth) ((Bot,West):history))
+turn :: GearState -> Maybe GearState
+turn (GearState face North tooth history) = Just (GearState face East tooth ((face,North):history))
+turn (GearState face East tooth history) = Just (GearState face North tooth ((face,East):history))
 
-forward (GearState Front South tooth history) = Just (GearState Bot North (incr tooth) ((Front,South):history))
-forward (GearState Front West tooth history) = Just (GearState Lef West (incr tooth) ((Front,West):history))
-
-forward (GearState Back South tooth history) = Just (GearState Bot South (incr tooth) ((Back,South):history))
-forward (GearState Back East tooth history) = Just (GearState Lef East (incr tooth) ((Back,East):history))
-forward (GearState Back West tooth history) = Just (GearState Ri West (incr tooth) ((Back,West):history))
-
-forward (GearState Lef North tooth history) = Just (GearState Top East (incr tooth) ((Lef,North):history))
-forward (GearState Lef East tooth history) = Just (GearState Front East (incr tooth) ((Lef,East):history))
-forward (GearState Lef West tooth history) = Just (GearState Back West (incr tooth) ((Lef,West):history))
-
-forward (GearState Ri North tooth history) = Just (GearState Top West (incr tooth) ((Ri,North):history))
-forward (GearState Ri South tooth history) = Just (GearState Bot East (incr tooth) ((Ri,South):history))
-forward (GearState Ri East tooth history) = Just (GearState Back East (incr tooth) ((Ri,East):history))
-
-forward _ = Nothing
-
-backward :: GearState -> Maybe GearState
-
-backward (GearState Top West tooth history) = Just (GearState Ri North (decr tooth) ((Top,West):history))
-backward (GearState Top East tooth history) = Just (GearState Lef North (decr tooth) ((Top,East):history))
-
-backward (GearState Bot South tooth history) = Just (GearState Back South (decr tooth) ((Bot,South):history))
-backward (GearState Bot North tooth history) = Just (GearState Front South (decr tooth) ((Bot,North):history))
-backward (GearState Bot East tooth history) = Just (GearState Ri South (decr tooth) ((Bot,East):history))
-
-backward (GearState Front North tooth history) = Just (GearState Bot South (decr tooth) ((Front,North):history))
-backward (GearState Front East tooth history) = Just (GearState Lef East (decr tooth) ((Front,East):history))
-
-backward (GearState Back North tooth history) = Just (GearState Bot North (decr tooth) ((Back,North):history))
-backward (GearState Back West tooth history) = Just (GearState Lef West (decr tooth) ((Back,West):history))
-backward (GearState Back East tooth history) = Just (GearState Ri East (decr tooth) ((Back,East):history))
-
-backward (GearState Lef South tooth history) = Just (GearState Top West (decr tooth) ((Lef,South):history))
-backward (GearState Lef West tooth history) = Just (GearState Front West (decr tooth) ((Lef,West):history))
-backward (GearState Lef East tooth history) = Just (GearState Back East (decr tooth) ((Lef,East):history))
-
-backward (GearState Ri South tooth history) = Just (GearState Top East (decr tooth) ((Ri,South):history))
-backward (GearState Ri North tooth history) = Just (GearState Bot West (decr tooth) ((Ri,North):history))
-backward (GearState Ri West tooth history) = Just (GearState Back West (decr tooth) ((Ri,West):history))
-
-backward _ = Nothing -}
-
+turn (GearState face South tooth history) = Just (GearState face West tooth ((face,South):history))
+turn (GearState face West tooth history) = Just (GearState face South tooth ((face,West):history))
 
 mod5 :: Int -> Int
 mod5 = (flip mod) 5
